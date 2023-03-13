14a7872ed99426149a1e45ce08090a8c08081f03
Misai Legara <99393784+iammisai@users.noreply.github.com>
2023-03-13 20:15:47 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -74,7 +74,7 @@ const config = {
             type: 'doc',
             docId: 'intro',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Get Started',
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
