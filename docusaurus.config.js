1dbd020147a1bb5c540eada29591750e3e47ae86
Misai Legara <99393784+iammisai@users.noreply.github.com>
2023-03-14 15:01:38 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -78,7 +78,7 @@ const config = {
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/teddy-swap',
             label: 'GitHub',
             position: 'right',
           },
@@ -118,11 +118,11 @@ const config = {
             items: [
               {
                 label: 'Blog',
-                to: '/blog',
+                href: 'https://blog.teddyswap.org',
               },
               {
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                href: 'https://github.com/teddy-swap',
               },
             ],
           },
