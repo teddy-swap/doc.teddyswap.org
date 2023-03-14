3b98566750d69fcbc12bd717dd52cdcaffcb2609
Misai Legara <99393784+iammisai@users.noreply.github.com>
2023-03-14 22:25:42 +0800

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -52,7 +52,8 @@ const sidebars = {
               'Swap',
               'Liquidity',
               'Redeem',
-              'Orders'
+              'Orders',
+              'Badger'
              ],
       collapsible: false
    },
