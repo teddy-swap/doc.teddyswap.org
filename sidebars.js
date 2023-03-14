4a0d2c79301c3ecd30313d81328384ad5b951633
Misai Legara <99393784+iammisai@users.noreply.github.com>
2023-03-14 16:50:26 +0800

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -25,7 +25,7 @@ const sidebars = {
     {
       type: 'category',
       label: 'Testnet Guide',
-      items: ['Testnet Guide/Getting Started'],
+      items: ['Testnet Guide/Getting Started/Testnet Guide'],
       collapsible: false
     }
   ],
