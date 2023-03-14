8ec61f95e2e4004725cd8b162ea73370cf0d96b1
Misai Legara <99393784+iammisai@users.noreply.github.com>
2023-03-14 16:31:49 +0800

    Create sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -18,8 +18,8 @@ const sidebars = {
     {
       type: 'category',
       label: 'Overview',
-      items: ['introduction', 'disclaimer', 'objectives', 'Advantages'],
-      //items: ['introduction', 'disclaimer', 'objectives', 'Advantages', 'Yield Opportunities', 'Roadmap', 'Security', 'Governance', 'Team'],
+     //items: ['introduction', 'disclaimer', 'objectives'],
+      items: ['introduction', 'disclaimer', 'objectives', 'Advantages', 'Product Summary', 'Yield Opportunities', 'Roadmap', 'Security', 'Tokenomics', 'Governance', 'Team'],
       collapsible: false
     },
     {
